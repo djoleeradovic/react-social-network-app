69d15dbe22d7419aa8a1bde3633bd4517c6af1a6
djoleeradovic <djoleeradovic@gmail.com>
2023-05-04 18:53:55 +0200

    social-network

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { AuthPage, Homepage } from "./Pages";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/react-social-media-app" element={<AuthPage />} />
         <Route path="/home" element={<Homepage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
