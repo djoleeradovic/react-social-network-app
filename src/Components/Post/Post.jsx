d3bab708970787403e4e3791674a1d2443bd7c30
djoleeradovic <djoleeradovic@gmail.com>
2023-05-10 14:04:44 +0200

    social-network

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,35 +1,67 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import "./post.css";
 import { AiOutlineLike, AiOutlineDislike } from "react-icons/ai";
 import { useCookies } from "react-cookie";
-import { setDataToID, deleteDataByID, getAllData } from "../../Helpers/Utils";
+import {
+  getDataByID,
+  setDataToID,
+  deleteDataByID,
+  getAllData,
+} from "../../Helpers/Utils";
 
 const Post = ({ id, user_id, content, likes, author, setPosts }) => {
-  const [liked, setLiked] = useState(false);
+  const [isLiked, setIsLiked] = useState([]);
   const [cookies] = useCookies(["user"]);
 
   const currentUser = cookies.user === user_id;
 
-  const setLike = (e) => {
-    e.preventDefault();
-    setLiked(true);
+  useEffect(() => {
+    const getLikedPosts = async () => {
+      const _ = await getDataByID("users", cookies.user);
+      setIsLiked(_.likedPosts);
+    };
+    getLikedPosts();
+  }, []);
 
-    const data = {
+  const setLike = async (e, postID) => {
+    e.preventDefault();
+    // Set post to users liked posts
+    const liked = await getDataByID("users", cookies.user);
+
+    const likedPosts = [...liked.likedPosts];
+
+    if (!likedPosts.includes(postID)) {
+      likedPosts.push(postID);
+    }
+    setIsLiked(likedPosts);
+
+    await setDataToID("users", cookies.user, { likedPosts });
+
+    // Update the like on post
+    const postLikes = {
       likes: likes + 1,
     };
-
-    setDataToID("posts", id, data);
+    await setDataToID("posts", id, postLikes);
   };
 
-  const setDislike = (e) => {
+  const setDislike = async (e, postID) => {
     e.preventDefault();
-    setLiked(false);
+    // Set post to users liked posts
+    const liked = await getDataByID("users", cookies.user);
 
-    const data = {
+    const likedPosts = [...liked.likedPosts];
+
+    if (likedPosts.includes(postID)) {
+      likedPosts.pop(postID);
+    }
+    setIsLiked(likedPosts);
+    await setDataToID("users", cookies.user, { likedPosts });
+
+    // Update the like on post
+    const postLikes = {
       likes: likes - 1,
     };
-
-    setDataToID("posts", id, data);
+    await setDataToID("posts", id, postLikes);
   };
 
   const deletePost = async (e, id) => {
@@ -46,18 +78,18 @@ const Post = ({ id, user_id, content, likes, author, setPosts }) => {
       <footer className="post-footer">
         <small className="post-author">Author: {author}</small>
         <div className="post-likes">
-          {liked ? (
+          {isLiked?.includes(id) ? (
             <AiOutlineDislike
               className="post-btn dislike"
               onClick={(e) => {
-                setDislike(e);
+                setDislike(e, id);
               }}
             />
           ) : (
             <AiOutlineLike
               className="post-btn like"
               onClick={(e) => {
-                setLike(e);
+                setLike(e, id);
               }}
             />
           )}
